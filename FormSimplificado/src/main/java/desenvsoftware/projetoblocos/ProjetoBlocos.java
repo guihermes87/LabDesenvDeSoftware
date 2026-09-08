d5ac28acea9ba1acff6d102317fb0a2d1269d96b
Guilherme Hermes <129190225+guihermes87@users.noreply.github.com>
2026-09-08 14:27:08 -0300

    Add files via upload

---
+++ FormSimplificado/src/main/java/desenvsoftware/projetoblocos/ProjetoBlocos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package desenvsoftware.projetoblocos;
+
+/**
+ *
+ * @author hermes
+ */
+public class ProjetoBlocos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
